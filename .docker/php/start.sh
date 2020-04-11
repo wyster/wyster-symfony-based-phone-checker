4318f95dd1939c74395b58b92d4d7bcc4bc07973
Ilya Zelenin <wyster@make.im>
2020-04-11 10:12:07 +0300

    feat: waiting mysql

--- .docker/php/start.sh
+++ .docker/php/start.sh
@@ -9,6 +9,8 @@ if [ $ENABLE_XDEBUG == "1" ]; then
     docker-php-ext-enable xdebug
 fi
 
+bash /wait-for.sh mysql:3306 -t 0 -- echo "Mysql started"
+
 php ./bin/console doctrine:migrations:migrate --no-interaction
 
 docker-php-entrypoint php-fpm
